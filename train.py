989ff51d1ec2e950b58973c667f4a888ca838d2e
lzx <919298566@qq.com>
2024-05-26 15:01:08 +0800

    Update README and train.py

--- train.py
+++ train.py
@@ -32,7 +32,7 @@ def parse_args():
     parser.add_argument('--replay_buffer_capacity', default=100000, type=int)
     # train
     parser.add_argument('--agent', default='resact', type=str, choices=['resact'])
-    parser.add_argument('--init_steps', default=10000, type=int)
+    parser.add_argument('--init_steps', default=1000, type=int)
     parser.add_argument('--num_train_steps', default=100000, type=int)
     parser.add_argument('--batch_size', default=128, type=int)
     parser.add_argument('--hidden_dim', default=1024, type=int)
